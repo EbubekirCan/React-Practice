a604c43697e0cc16ad73e455181ccc5ab56536a3
Ebubekir <ebubekirc442@gmail.com>
2023-09-08 18:00:49 +0300

    08.09.23

---
+++ myapp/src/components/c1/Ruya.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ruya = () => {
+  return (
+    <div>Ruya: Ben sadece müzik dersi aliyorum   </div>
+  )
+}
+
+export default Ruya
---
+++ myapp/src/components/c1/Sevgi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sevgi = () => {
+  return (
+    <div>Sevgi: ben matematik ve müzik dersi alıyorum</div>
+  )
+}
+
+export default Sevgi
