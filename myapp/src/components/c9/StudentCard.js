e18b339e98c26b46fc888d8476294a5bb31d4e1c
Ebubekir <ebubekirc442@gmail.com>
2023-09-13 17:48:59 +0300

    13.09.23

--- myapp/src/components/c9/StudentCard.js
+++ myapp/src/components/c9/StudentCard.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import {Container} from "react-bootstrap"
+import data from "../../assets/data/students.json"
+import { Col, Container, Row} from "react-bootstrap"
+import Kartlar from './Kartlar'
+import "./card.scss"
+
+
 
 const StudentCard = () => {
   return (
-    <Container> </Container>
+
+    <Container className='text-center'>
+        <Row className='mb-3'>
+
+            {
+
+                data.map((student,index)=><Col key={index} md={6} lg={3} xl={4}  >
+                <Kartlar {...student} />
+                </Col>)
+            }
+        </Row>
+    </Container>
+    
   )
 }
 
