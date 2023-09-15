6bf65aea3f6577cf562ef5467e396f91d49fc908
Ebubekir <ebubekirc442@gmail.com>
2023-09-15 17:27:05 +0300

    15.09.23

--- myapp/src/components/c11/RandomImage.js
+++ myapp/src/components/c11/RandomImage.js
@@ -1,8 +1,18 @@
- 
+import {Card , Button} from "react-bootstrap"
 const RandomImage = () => {
   return (
-    <div>RandomImage</div>
-  )
-}
+    <Card style={{ width: "18rem",  textAlign:"center" ,margin:"5rem auto "}}  >
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default RandomImage
+export default RandomImage;
