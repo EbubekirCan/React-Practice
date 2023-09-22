f1f823063046150660b0322bc5f2655b45f11c32
Ebubekir <ebubekirc442@gmail.com>
2023-09-22 18:05:30 +0300

    22.09.23

--- myapp/src/components/c16/BusinessInfo.js
+++ myapp/src/components/c16/BusinessInfo.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { RingLoader } from 'react-spinners'
+import CardBusiness from './CardBusiness'
 
 const BusinessInfo = () => {
     const [data, setdata] = useState([])
@@ -6,30 +9,61 @@ const BusinessInfo = () => {
 
     useEffect(() => {
 
-        const getBusinessData =async () => { 
+        const getBusinessData =() => { 
 
-            const resp= await fetch("https://650d9fdda8b42265ec2c80a4.mockapi.io/products")
+        const timer=    setTimeout(()=>{
+                 fetch("https://650d9fdda8b42265ec2c80a4.mockapi.io/products")
             .then((res)=>res.json())
-            .then((data)=>setdata(resp.data))
-            console.log(resp.data)
+            .then((data)=>{
+                setdata(data)
+                   console.log(data)
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+
+            .finally(()=>{
+                setloading(false)
+            })
 
 
+            },3000)
+
+         
+
+return(()=>{
+    clearTimeout(timer)
+})
+         
 
          }
 
 
-
-    
     
       return () => {
-        getBusinessData()
-    
+    getBusinessData()
       }
     }, [])
     
 
   return (
-    <div>BusinessInfo</div>
+    <Container className='mt-5 text-center' >
+        {
+        loading && <RingLoader color="#36d7b7" />}
+        <Row className='g-5'>
+            {
+                data.map((item)=><Col key={item.id} sm={6} md={4} lg={3} xl={2} >
+
+                    <CardBusiness {...item} />
+                
+                </Col>)
+            }
+        </Row>
+        
+
+
+
+    </Container>
   )
 }
 
