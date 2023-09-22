b6a060712d3857935cede0648dc7ac94a06626b6
Ebubekir <ebubekirc442@gmail.com>
2023-09-22 17:32:27 +0300

    22.09.23

--- myapp/src/components/c16/BusinessInfo.js
+++ myapp/src/components/c16/BusinessInfo.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const BusinessInfo = () => {
+    const [data, setdata] = useState([])
+    const [loading, setloading] = useState(true)
+
+    useEffect(() => {
+
+        const getBusinessData =async () => { 
+
+            const resp= await fetch("https://650d9fdda8b42265ec2c80a4.mockapi.io/products")
+            .then((res)=>res.json())
+            .then((data)=>setdata(resp.data))
+            console.log(resp.data)
+
+
+
+         }
+
+
+
+    
+    
+      return () => {
+        getBusinessData()
+    
+      }
+    }, [])
+    
+
   return (
     <div>BusinessInfo</div>
   )
