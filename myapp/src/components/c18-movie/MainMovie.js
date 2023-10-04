4a6029d320cd7cc903479889d449efcc8e10dc77
Ebubekir <ebubekirc442@gmail.com>
2023-10-04 17:27:03 +0300

    04.10.23

--- myapp/src/components/c18-movie/MainMovie.js
+++ myapp/src/components/c18-movie/MainMovie.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+
 const MainMovie = () => {
-  return (
+    let API_KEY="&api_key=f98c87a47c745ba4ef7443f0507475e4"
+let BASE_URL="https://api.themoviedb.org/3";
+   return (
     <div>MainMovie</div>
   )
 }
